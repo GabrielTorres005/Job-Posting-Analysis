d937d2b95e68a0bc906eb23cd77c9676db931182
GabrielTorres005 <gabriel.torres0228@gmail.com>
2026-08-10 00:44:53 -0500

    Updated first query to match project goals

---
+++ 01_top_demanded_internship_role.sql
@@ -0,0 +1,47 @@
+/*
+    --Evaluate demand volume across entry-level/intern data and tech roles for summer 2025--
+    
+    
+*/
+
+SELECT
+  job_title_short AS internship_role,
+  COUNT(*) AS demand_count
+FROM job_postings_fact
+WHERE 
+  (job_title LIKE '%intern%' OR job_title LIKE '%Intern%')
+  AND (job_posted_date >= '2024-07-01 00:00:00' AND job_posted_date <= '2025-05-31 23:59:00')
+  AND (job_title_short NOT LIKE '%Senior%')
+GROUP BY
+  internship_role
+ORDER BY
+  demand_count DESC;
+
+
+
+/*
+    Result:
+
+┌───────────────────────────┬──────────────┐
+│      internship_role      │ demand_count │
+│          varchar          │    int64     │
+├───────────────────────────┼──────────────┤
+│ Data Analyst              │         4239 │
+│ Data Scientist            │         2171 │
+│ Data Engineer             │         1257 │
+│ Business Analyst          │          525 │
+│ Software Engineer         │          285 │
+│ Machine Learning Engineer │          221 │
+│ Cloud Engineer            │           39 │
+└───────────────────────────┴──────────────┘
+  7 rows         2 columns
+
+
+Key Takeaways:
+    -  Data Dominance: Nearly half of all postings (48.52%), Data Analyst is overwhelmingly the 
+    most accessible and heavily requested internship role. Combined demand for DA, DS, and DE reaches 7,667 positions (87.75%).
+    There high corporate interest in predictive modeling and advanced analytics in the talent pipeline.
+
+    - Specialized vs Generealist engineering gap:S pecialized infrastructure/advanced AI roles maintain lower internship volume,
+      suggesting companies prefer hiring senior/full-time talent for complex cloud architecture and ML ops rather than interns.
+*/
